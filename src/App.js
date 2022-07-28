15220eb07b7fc785c8e13ddb954830f328824525
IjazkhanAfridi <iafridi640@gmail.com>
2022-07-28 18:28:28 +0500

    Error Resolve

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Box, Stack ,ThemeProvider ,createTheme} from '@mui/material'
+import Addpost from './Component/Addpost';
+import Navbar from './Component/Navbar'
+import Lsidebar from './Component/Lsidebar'
+import Feed from './Component/Feed';
+import Rsidebar from './Component/Rsidebar';
+import { useState } from 'react';
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <Box bgcolor={"background.default"} color={"text.primary"}>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Lsidebar setMode={setMode} mode={mode}/>
+        <Feed />
+        <Rsidebar />
+      </Stack>
+      <Addpost />
+    </Box>
+    </ThemeProvider>
   );
 }
 
